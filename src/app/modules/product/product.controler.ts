45cb9ad1fe972cc8f2427aab7324bd6d467b4773
1Md-Rakibul-Islam <rakibul.islam.developer@gmail.com>
2024-11-13 02:23:36 +0600

    product get api added

--- src/app/modules/product/product.controler.ts
+++ src/app/modules/product/product.controler.ts
@@ -2,9 +2,10 @@ import { Request, Response } from "express";
 import { ProductServices } from "./product.service";
 import productValidationSchema from "./product.validation";
 
+// create a new product controller
 const createProduct = async (req: Request, res: Response) => {
     try {
-        const { product: productData } = req.body;
+        const productData = req.body;
 
         const zodParseData = productValidationSchema.parse(productData);
 
@@ -26,6 +27,31 @@ const createProduct = async (req: Request, res: Response) => {
     }
 };
 
+// get all products controller
+const getAllProducts = async (req: Request, res: Response) => {
+    try {
+
+        const result = await ProductServices.getAllProductsIntoDb();
+
+        res.status(200).json({
+            success: true,
+            message: "Products fetched successfully",
+            data: result
+        })
+
+    } catch (error) {
+
+        res.status(500).json({
+            success: false,
+            message: (error as Error) || "Products fetching failed",
+            error: error
+        })
+
+    };
+}
+
+
 export const ProductController = {
     createProduct,
+    getAllProducts,
 }
--- src/app/modules/product/product.service.ts
+++ src/app/modules/product/product.service.ts
@@ -1,6 +1,7 @@
 import { Product } from "./product.model";
 import { TProduct } from "./product.interface";
 
+// create product
 const createProductIntoDb = async (productData: TProduct) => {
 
     const result = await Product.create(productData);
@@ -8,6 +9,16 @@ const createProductIntoDb = async (productData: TProduct) => {
     return result;
 };
 
+// get all products
+const getAllProductsIntoDb = async () => {
+
+    const result = await Product.find({});
+
+    return result;
+
+};
+
 export const ProductServices = {
     createProductIntoDb,
+    getAllProductsIntoDb,
 }
